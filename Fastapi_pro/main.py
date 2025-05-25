b2f305427b5dc3634a8e9449e456ea559f3d7612
杨建萍 <369255095@qq.com>
2025-05-25 17:45:28 +0300

    5-25

--- Fastapi_pro/main.py
+++ Fastapi_pro/main.py
@@ -1,8 +1,15 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    a = "a"
+    b = "b" + a
+    return {"hello world": b}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
